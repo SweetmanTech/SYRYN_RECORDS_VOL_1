26b7ed053046ea2a7e1d4a23382b5bc852a1ce16
Sweets Sweetman <sweetmantech@gmail.com>
2022-11-10 22:48:42 -0300

    I update Track Player ordering on mobile.

--- components/HomePage/HomePage.tsx
+++ components/HomePage/HomePage.tsx
@@ -24,10 +24,10 @@ const HomePage: NextPage<HomePageProps> = ({ collection, chainId }) => {
     <>
       <SeoHead />
       <div className="flex grid grid-cols-6 p-5 justify-center align-center" style={{backgroundColor: "#ceb435"}}> 
-          <div className="flex col-span-6 md:col-span-3 justify-center">
+          <div className="order-1 flex col-span-6 md:col-span-3 justify-center">
             <img className="lg:max-w-lg" src="/images/syryn_records2.png" />
           </div>   
-          <div className="flex flex-col justify-end text-xs md:text-2xl col-span-6 md:col-span-3 gap-5 pb-5">
+          <div className="order-2 flex flex-col justify-end text-xs md:text-2xl col-span-6 md:col-span-3 gap-5 pb-5">
             <p>
               Syryn Records is a <span className="font-bold">youth-run record label</span> serving young women and gender-expansive artists.
             </p>
@@ -35,10 +35,10 @@ const HomePage: NextPage<HomePageProps> = ({ collection, chainId }) => {
               Women and non-binary professionals are underrepresented in the music industry - We want to change that! Our mission as Syryn Records is to <span className="italic font-bold">uplift</span> and <span className="italic font-bold">empower</span> youth interested in music industry professionals. We run yearly internships where our interns support and nurture a group of talented youth artists.
             </p>
           </div>
-          <div className="col-span-3 flex justify-center items-center">
+          <div className="order-3 col-span-3 flex justify-center items-center">
             <img className="lg:max-w-sm" src="/images/album_art2.png" />
           </div>
-          <div className="flex flex-col justify-start text-xs md:text-2xl col-span-3">
+          <div className="order-4 flex flex-col justify-start text-xs md:text-2xl col-span-3">
             <p className="pb-5">
               Each year, we create a <span className="font-bold">compilation record</span> that supports our artists and our program. This year&apos;s record features 11 talented female musicians, poets, songwriters, and producers.
             </p>
@@ -46,7 +46,7 @@ const HomePage: NextPage<HomePageProps> = ({ collection, chainId }) => {
               This year, we wanted to introduce our interns and artists to the incredible world of web3 and music NFTs! We are proud to introduce our genesis (aka first!) music NFT - an audio player featuring our compilation record, Syryn Records Vol. I!
             </p>
           </div>          
-          <div className="col-span-6 lg:col-span-3">
+          <div className="order-6 lg:order-5 col-span-6 lg:col-span-3">
             <div className="flex flex-col gap-3">
               <p className="font-bold">
                 Track Listing            
@@ -89,7 +89,7 @@ const HomePage: NextPage<HomePageProps> = ({ collection, chainId }) => {
             <img className="sm:w-full md:w-1/2" src="/images/purchasing_track_list.png" />
 
           </div>
-          <div className="flex flex-col justify-center text-xs md:text-lg col-span-6 lg:col-span-3">
+          <div className="order-5 lg:order-6 flex flex-col justify-center text-xs md:text-lg col-span-6 lg:col-span-3">
             <ERC721DropContractProvider
                 erc721DropAddress={collection.address}
                 chainId={chainId}
@@ -113,10 +113,10 @@ const HomePage: NextPage<HomePageProps> = ({ collection, chainId }) => {
             </Well>
             </ERC721DropContractProvider>
           </div>
-          <div className="col-span-6 pt-5 flex justify-center">
+          <div className="order-7 col-span-6 pt-5 flex justify-center">
             <img className="sm:w-full md:w-1/2" src="/images/our_artists2.png" />
           </div>
-          <div className="col-span-6 grid flex justify-around grid-cols-1 md:grid-cols-4">
+          <div className="order-8 col-span-6 grid flex justify-around grid-cols-1 md:grid-cols-4">
             <img src="/images/salome.png" />
             <img src="/images/geia.png" />
             <img src="/images/a_kai.png" />
